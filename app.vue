b61c0e86f12a25e22391017eef7906799710f5ee
Johannes Grupp <work@jgrp.de>
2025-06-07 18:04:18 +0200

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtPage />
+<!--    <NuxtRouteAnnouncer />-->
+<!--    <NuxtWelcome />-->
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
+})
